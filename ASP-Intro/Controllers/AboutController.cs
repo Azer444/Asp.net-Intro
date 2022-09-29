4cd08782e7f00b161dfeaf231221b56756a5006b
Azer444 <azerrh@code.edu.az>
2022-09-30 02:33:43 +0400

    Asp.net intro

---
+++ ASP-Intro/Controllers/AboutController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_Intro.Controllers
+{
+    public class AboutController
+    {
+        public string Index()
+        {
+            return "Salam P130";
+        }
+
+        public int Age()
+        {
+            return 21;
+        }
+
+
+    }
+}
---
+++ ASP-Intro/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_Intro.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+        //public string Index(int id)
+        //{
+        //    return "Salam Azer" + "-" + id;
+        //}
+
+        //public IActionResult Index()
+        //{
+        //    return Content("Salamlar");
+        //}
+
+        //public string FullData(string name, string surname, int age)
+        //{
+        //    return $"Name - {name} Surname - {surname} Age - {age}";
+        //}
+
+        //public IActionResult Index()
+        //{
+        //    return Json(new { name = "Azer", surname = "Humbetov" });
+        //}
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+    }
+}

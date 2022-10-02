449819c79a1e62d34d72881239cfb94dae7b3275
Azer444 <azerrh@code.edu.az>
2022-10-03 02:45:03 +0400

    HomeWork

--- ASP-Intro/Controllers/ProductController.cs
+++ ASP-Intro/Controllers/ProductController.cs
@@ -6,23 +6,11 @@ using System.Threading.Tasks;
 
 namespace ASP_Intro.Controllers
 {
-    public class AboutController:Controller
+    public class ProductController : Controller
     {
-        //public string Index()
-        //{
-        //    return "Salam P130";
-        //}
-
-        public int Age()
-        {
-            return 21;
-        }
-
         public IActionResult Index()
         {
             return View();
         }
-
-
     }
 }
